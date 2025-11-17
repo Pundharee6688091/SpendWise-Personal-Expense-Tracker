45ad9bc9e163bbd94cb23b5e40388f1584e7c760
Yok <yokojournal.co@gmail.com>
2025-11-17 17:42:18 +0700

    back button

--- spend_wise/lib/page/transaction.dart
+++ spend_wise/lib/page/transaction.dart
@@ -181,6 +181,14 @@ class _TransactionsScreenState extends State<TransactionsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black, size: 20),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 24.0),
